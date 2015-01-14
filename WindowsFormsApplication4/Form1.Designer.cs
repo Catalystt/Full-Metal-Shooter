650b28b172ffb3372d2b044cf1af871ee28fac39
godicgamer <pvp_gamer@hotmail.com>
2015-01-14 15:09:33 -0500

    Full Metal Shooter version: 0.01
    Done:
    Smooth Movement
    
    Planning:
    Projectile Movement
    Enemy AI
    Level Creation
    Power-Ups

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.shooting = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.moveshot = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(515, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // shooting
+            // 
+            this.shooting.Interval = 25;
+            this.shooting.Tick += new System.EventHandler(this.shooting_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication4.Properties.Resources.Ed_chibi_vector_by_zsofi1989;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApplication4.Properties.Resources.fireball;
+            this.pictureBox2.Location = new System.Drawing.Point(603, 149);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(99, 60);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(649, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // moveshot
+            // 
+            this.moveshot.Interval = 25;
+            this.moveshot.Tick += new System.EventHandler(this.moveshot_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1033);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.MinimumSize = new System.Drawing.Size(1080, 720);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.PeachPuff;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer shooting;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer moveshot;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        System.Windows.Forms.Timer keyManagerTimer = new System.Windows.Forms.Timer();
+        List<PictureBox> list=new List<PictureBox>();
+        List<double> xcoord = new List<double>();
+        List<double> ycoord = new List<double>();
+        List<double> degree = new List<double>();
+        List<double> xdistance = new List<double>();
+        List<double> ydistance = new List<double>();
+        int currentshot=0;
+        double range = 250;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.keyManagerTimer.Tick += (s, e) => Movement();
+            this.keyManagerTimer.Interval = 25;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BringToFront();
+            this.Focus();
+            this.KeyPreview = true;
+            this.MouseDown += new MouseEventHandler(Form1_MouseDown);
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if ((keyData & Keys.Right) != 0)
+            {
+                keyManagerTimer.Enabled = true;
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void Movement()
+        {
+            bool isDownKeyPressed = IsKeyPressed(Keys.S);
+            bool isRightKeyPressed = IsKeyPressed(Keys.D);
+            bool isUpKeyPressed = IsKeyPressed(Keys.W);
+            bool isLeftKeyPressed = IsKeyPressed(Keys.A);
+
+            //Case for Down and Right
+            if (isRightKeyPressed && !isDownKeyPressed)
+            {
+                if (pictureBox1.Left < 1080 )  pictureBox1.Left += 8;
+            }
+            if (!isRightKeyPressed && isDownKeyPressed)
+            {
+                if (pictureBox1.Top < 720) pictureBox1.Top += 8;
+            }
+            if (isRightKeyPressed && isDownKeyPressed)
+            {
+                if (pictureBox1.Top <720) pictureBox1.Top += 8;
+                if (pictureBox1.Left < 1080)  pictureBox1.Left += 8;
+            }
+
+            //Case for Right and Up
+            if (isRightKeyPressed && !isUpKeyPressed)
+            {
+                if (pictureBox1.Left <1080) pictureBox1.Left += 8;
+            }
+            if (!isRightKeyPressed && isUpKeyPressed)
+            {
+                if (pictureBox1.Top > 0) pictureBox1.Top -= 8;
+            }
+            if (isRightKeyPressed && isUpKeyPressed)
+            {
+                if (pictureBox1.Top > 0) pictureBox1.Top -= 8;
+                if (pictureBox1.Left < 1080) pictureBox1.Left += 8;
+            }
+
+            //Case for Left and Up
+            if (isLeftKeyPressed && !isUpKeyPressed)
+            {
+                if (pictureBox1.Left > 0) pictureBox1.Left -= 8;
+            }
+            if (!isLeftKeyPressed && isUpKeyPressed)
+            {
+                if (pictureBox1.Top > 0) pictureBox1.Top -= 8;
+            }
+            if (isLeftKeyPressed && isUpKeyPressed)
+            {
+                if (pictureBox1.Top > 0) pictureBox1.Top -= 8;
+                if (pictureBox1.Left > 0) pictureBox1.Left -= 8;
+            }
+
+            //Case for Left and Up
+            if (isLeftKeyPressed && !isDownKeyPressed)
+            {
+                if (pictureBox1.Left > 0) pictureBox1.Left -= 8;
+            }
+            if (!isLeftKeyPressed && isDownKeyPressed)
+            {
+                if (pictureBox1.Top < 720) pictureBox1.Top += 8;
+            }
+            if (isLeftKeyPressed && isDownKeyPressed)
+            {
+                if (pictureBox1.Top < 720) pictureBox1.Top += 8;
+                if (pictureBox1.Left > 0) pictureBox1.Left -= 8;
+            }
+        }
+
+        //Slow Movement and NO Possiblity for KeyDown if another key is already held
+        //void Form1_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    if (e.KeyCode==Keys.Right || e.KeyCode==Keys.D)
+        //    {
+        //        if (pictureBox1.Left < 708)
+        //        {
+        //            pictureBox1.Left += 8;
+        //        }
+        //    }
+        //    
+        //    if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+        //    {
+        //        if (pictureBox1.Left > 0)
+        //        {
+        //            pictureBox1.Left -= 8;
+        //        }
+        //    }
+        //
+        //    if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+        //    {
+        //         if (pictureBox1.Top > 0)
+        //         {
+        //             pictureBox1.Top -= 8;
+        //         }
+        //    }
+        //
+        //    if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+        //    {
+        //        if (pictureBox1.Top < 580)
+        //        {
+        //            pictureBox1.Top += 8;
+        //        }
+        //    }
+        //    
+        //    if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S && e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+        //    {
+        //        if (pictureBox1.Top < 580)  pictureBox1.Top += 8;
+        //        if (pictureBox1.Left > 0)   pictureBox1.Left -= 8;
+        //
+        //    }
+        //}
+
+
+        private void Shoot()
+        {
+
+            
+
+        }
+        public static bool IsKeyPressed(Keys key)
+        {
+            return BitConverter.GetBytes(GetKeyState((int)key))[1] > 0;
+        }
+
+        [DllImport("user32")]
+        private static extern short GetKeyState(int vKey);
+
+        private void bullets_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                PictureBox bullet=new PictureBox();
+                bullet.Enabled = true;
+                bullet.Visible = true;
+                bullet.Height = 43;
+                bullet.Width = 41;
+                bullet.Location = new Point(pictureBox1.Left, pictureBox1.Top);
+                bullet.Image = new Bitmap(@"C:\Users\Jason\Pictures\fireball.png");
+                bullet.SizeMode = PictureBoxSizeMode.StretchImage;
+                Controls.Add(bullet);
+                bullet.BringToFront();
+
+                list.Add(bullet);
+                xcoord.Add(this.PointToClient(Cursor.Position).X - pictureBox1.Left);
+                ycoord.Add(-1*(this.PointToClient(Cursor.Position).Y - pictureBox1.Top));
+                degree.Add(Math.Atan2(ycoord[currentshot], xcoord[currentshot]) * 180 / Math.PI);
+                if (Cursor.Position.X >= pictureBox1.Left)
+                {
+                    xdistance.Add(range * Math.Cos(degree[currentshot]));
+                }
+                else xdistance.Add(-(range * Math.Cos(degree[currentshot])));
+                
+                ydistance.Add(range * Math.Sin(degree[currentshot]));
+                label1.Text = Convert.ToString( degree[currentshot]);
+                label2.Text = Convert.ToString(ydistance[currentshot]);
+                
+                shooting.Enabled = true;
+                currentshot++;
+            }
+
+        }
+
+        private void shooting_Tick(object sender, EventArgs e)
+        {
+
+            //label2.Text = Convert.ToString(range*Math.Cos(degree[currentshot]));  
+            shooting.Enabled = false;       
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void moveshot_Tick(object sender, EventArgs e)
+        {
+
+
+            moveshot.Enabled = false;
+        }
+
+    }
+}
